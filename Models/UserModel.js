768748ea69ca522ee5f2e8abd9f4a7640ae87b63
tarekbinjaman <tarekbinjaman@gmail.com>
2025-09-23 20:45:40 +0600

    user model schema updated

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,45 +2,45 @@ const { Schema, model } = require("mongoose");
 const { link } = require("../Routes/users");
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 50,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    number_of_Product_added: {
-        type: Number,
-        default: false
-    },
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  number_of_Product_added: {
+    type: Number,
+    default: false,
+  },
 
-    agree: {
-        type: Boolean,
-        default: false
+  agree: {
+    type: Boolean,
+    default: false,
+  },
+  photoURL: {
+    type: String,
+    trim: true,
+  },
+  useraddress: [
+    {
+      address: { type: String, required: true, default: "" },
+      number: { type: Number, required: true, default: 0 },
     },
-    photoURL: {
-        type: String,
-        trim: true
-    },
-    useraddress: {
-        type: String,
-        trim: true,
-        default: ""
-    }
-
+  ],
 });
 
-const userModel = model("user", userSchema)
-module.exports = userModel
+const userModel = model("user", userSchema);
+module.exports = userModel;
