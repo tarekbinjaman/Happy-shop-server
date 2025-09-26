3f112c6a1890e3676a60bb393fd711aabefc3f57
tarekbinjaman <tarekbinjaman@gmail.com>
2025-09-26 15:27:31 +0600

    order schema added perfectly

---
+++ Models/Order.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = new Schema({
+  userEmail: { type: String, required: true },
+  item: [
+    {
+      productId: { type: String, required: true },
+      title: { type: String, required: true },
+      color: { type: String, required: true },
+      price: { type: Number, required: true },
+      size: { type: String, required: true },
+      image: { type: String, required: true },
+    },
+  ],
+  shippingAddress: {
+    name: {type: String, required: true},
+    address: {type: String, required: true},
+    phone: {type: Number, required: true}
+  },
+  status: {type: String, required: true},
+  subTotal: {type: Number, required: true},
+  shippingCost: {type: Number, required: true},
+  totalAmount: {type: Number, required: true},
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const orderModal = model("order", orderSchema);
+module.exports = orderModal;
